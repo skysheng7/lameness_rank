3548d1dafdecde6071a16c20e4abd9a26d00d207
Sky <82303914+skysheng7@users.noreply.github.com>
2023-09-19 09:19:37 -0700

    organized generate 30 cow GS

--- 02-generate_30cow_GS_label_html_experts/code/Create_gs_labeling_HTML.py
+++ 02-generate_30cow_GS_label_html_experts/code/Create_gs_labeling_HTML.py
@@ -6,100 +6,14 @@ from datetime import datetime
 import itertools
 import numpy as np
 from datetime import date
+exec(open("Create_gs_labeling_HTML_helper.py").read())
 
 today = date.today().strftime("%b-%d-%Y")
-envir = "Sky"
-if (envir == "Sora"):
-    folder_path = '/Users/sora/Library/CloudStorage/OneDrive-UBC/Sora Jeong/results/30cow_artificial_group_compressed'
-    input_dir = '/Users/sora/Library/CloudStorage/OneDrive-UBC/Sora Jeong/code/html modify code/input file'
-    output_dir = '/Users/sora/Library/CloudStorage/OneDrive-UBC/Sora Jeong/results/html'
-else:
-    folder_path = '/Users/skysheng/Library/CloudStorage/OneDrive-UBC/University of British Columbia/Research/PhD Project/Amazon project phase 2/Sora Jeong/results/30cow_artificial_group_compressed'
-    input_dir = '/Users/skysheng/Library/CloudStorage/OneDrive-UBC/University of British Columbia/Research/PhD Project/Amazon project phase 2/code/gs_labeling_html/input file'
-    output_dir = '/Users/skysheng/Library/CloudStorage/OneDrive-UBC/University of British Columbia/Research/PhD Project/Amazon project phase 2/results/gs_labeling_html'
 
-################################################################
-############## Generate HTML of the test HIT ###################
-################################################################
-# create a function to generate HTML once having the question list
 
-def generate_html(input_dir, output_dir, df, total_ques, cur_hit, today):
-    """
-    ########################## p1 css processing ##############################
-    """
-    os.chdir(input_dir)
-    with open(r'p1_css.txt', 'r') as file:
-        p1_css = file.read()
-
-    p1_css_mod = p1_css.replace("${total_q_num}", str(total_ques))
-    p1_css_mod = p1_css_mod + "\n"
-
-    """
-    ######################### p2 html question processing #########################
-    """
-    video1 = df['URL'].tolist()
-
-    with open(r'p2_html_q1.txt', 'r') as file:
-        p2_html_q1 = file.read()
-    with open(r'p2_html_other_q.txt', 'r') as file:
-        p2_html_other = file.read()
-
-    for n in range(total_ques):
-        if n == 0:
-            cur_q = p2_html_q1
-        else:
-            cur_q = p2_html_other
-
-        cur_q = cur_q.replace("${cur_question}", str(n + 1))
-        cur_q = cur_q.replace("${total_q_num}", str(total_ques))
-        cur_q = cur_q.replace("${video1}", video1[n])
-
-        if n == 0:
-            p2_html_mod = cur_q + "\n"
-        else:
-            p2_html_mod = p2_html_mod + cur_q + "\n"
-
-    """
-    ########################### p3 java script processing #########################
-    """
-    with open(r'p3_js.txt', 'r') as file:
-        p3_js = file.read()
-
-    p3_js_mod = p3_js.replace("${total_q_num}", str(total_ques))
-    p3_js_mod = p3_js.replace("${total_q_num_js}", str((total_ques+2)))
-
-    q_eqs = """const question$ = document.getElementById("question$")"""
-    q_list = "question$,"
-
-    for n in range(total_ques):
-        cur_q_eqs = q_eqs
-        cur_q = q_list
-        cur_q_eqs = cur_q_eqs.replace("$", str(n + 1))
-        cur_q = cur_q.replace("$", str(n + 1))
-
-        if n == 0:
-            all_eqs = cur_q_eqs
-            all_q = cur_q
-        else:
-            all_eqs = all_eqs + "\n" + cur_q_eqs
-            all_q = all_q + "\n" + cur_q
-
-    p3_js_mod = p3_js_mod.replace("${q_equs}", all_eqs)
-    p3_js_mod = p3_js_mod.replace("${q_list}", all_q)
-
-    """
-    ###################### merge all parts into 1 complete html ###################
-    """
-    merged_html = p1_css_mod + p2_html_mod + p3_js_mod
-
-    """
-    ################################ EXPORT result ################################
-    """
-    os.chdir(output_dir)
-    output_file = 'GS_30cows_HIT' + str(cur_hit) + '_' + today + '.html'
-    with open(output_file, 'w') as f:
-        f.write(merged_html)
-
+folder_path = '/Users/skysheng/Library/CloudStorage/OneDrive-UBC/University of British Columbia/Research/PhD Project/Amazon project phase 2/Sora Jeong/results/30cow_artificial_group_compressed'
+input_dir = './input file'
+output_dir = '../results'
 
 ################################################################
 ############ TASK 1: create URL for each video files ###########
@@ -133,7 +47,7 @@ df = df.sample(frac=1, random_state=180).reset_index(drop=True)
 df['question_num'] = ['q'+str(i) for i in range(1,(df.shape[0]+1))]
 
 ################################################################
-######### Create a test HIT with 12 questions ##################
+######### Create a test HIT with 30 GS questions ###############
 ################################################################
 # set HIT number
 cur_hit = 0
---
+++ 02-generate_30cow_GS_label_html_experts/code/Create_gs_labeling_HTML_helper.py
@@ -0,0 +1,82 @@
+################################################################
+############## Generate HTML of the test HIT ###################
+################################################################
+# create a function to generate HTML once having the question list
+
+def generate_html(input_dir, output_dir, df, total_ques, cur_hit, today):
+    """
+    ########################## p1 css processing ##############################
+    """
+    
+    with open((input_dir + r'/p1_css.txt'), 'r') as file:
+        p1_css = file.read()
+
+    p1_css_mod = p1_css.replace("${total_q_num}", str(total_ques))
+    p1_css_mod = p1_css_mod + "\n"
+
+    """
+    ######################### p2 html question processing #########################
+    """
+    video1 = df['URL'].tolist()
+
+    with open((input_dir + r'/p2_html_q1.txt'), 'r') as file:
+        p2_html_q1 = file.read()
+    with open((input_dir + r'/p2_html_other_q.txt'), 'r') as file:
+        p2_html_other = file.read()
+
+    for n in range(total_ques):
+        if n == 0:
+            cur_q = p2_html_q1
+        else:
+            cur_q = p2_html_other
+
+        cur_q = cur_q.replace("${cur_question}", str(n + 1))
+        cur_q = cur_q.replace("${total_q_num}", str(total_ques))
+        cur_q = cur_q.replace("${video1}", video1[n])
+
+        if n == 0:
+            p2_html_mod = cur_q + "\n"
+        else:
+            p2_html_mod = p2_html_mod + cur_q + "\n"
+
+    """
+    ########################### p3 java script processing #########################
+    """
+    with open((input_dir + r'/p3_js.txt'), 'r') as file:
+        p3_js = file.read()
+
+    p3_js_mod = p3_js.replace("${total_q_num}", str(total_ques))
+    p3_js_mod = p3_js.replace("${total_q_num_js}", str((total_ques+2)))
+
+    q_eqs = """const question$ = document.getElementById("question$")"""
+    q_list = "question$,"
+
+    for n in range(total_ques):
+        cur_q_eqs = q_eqs
+        cur_q = q_list
+        cur_q_eqs = cur_q_eqs.replace("$", str(n + 1))
+        cur_q = cur_q.replace("$", str(n + 1))
+
+        if n == 0:
+            all_eqs = cur_q_eqs
+            all_q = cur_q
+        else:
+            all_eqs = all_eqs + "\n" + cur_q_eqs
+            all_q = all_q + "\n" + cur_q
+
+    p3_js_mod = p3_js_mod.replace("${q_equs}", all_eqs)
+    p3_js_mod = p3_js_mod.replace("${q_list}", all_q)
+
+    """
+    ###################### merge all parts into 1 complete html ###################
+    """
+    merged_html = p1_css_mod + p2_html_mod + p3_js_mod
+
+    """
+    ################################ EXPORT result ################################
+    """
+    output_file = 'GS_30cows_HIT' + str(cur_hit) + '_' + today + '.html'
+    with open((output_dir + '/' + output_file), 'w') as f:
+        f.write(merged_html)
+
+
