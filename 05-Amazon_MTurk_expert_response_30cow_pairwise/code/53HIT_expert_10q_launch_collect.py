b5c11f4b564b4db49569b9b65f416637b86c094d
Sky <82303914+skysheng7@users.noreply.github.com>
2023-10-01 16:17:50 -0700

    SB, Ruan HIT launch

--- 05-Amazon_MTurk_expert_response_30cow_pairwise/code/53HIT_expert_10q_launch_collect.py
+++ 05-Amazon_MTurk_expert_response_30cow_pairwise/code/53HIT_expert_10q_launch_collect.py
@@ -47,7 +47,7 @@ today = date.today().strftime("%b-%d-%Y")
 
 input_dir = "04-generate_54HIT_html_experts/results"
 key_dir ='/Users/skysheng/Library/CloudStorage/OneDrive-UBC/University of British Columbia/Research/PhD Project/Amazon project phase 2/data/Amazon MTurk'
-output_dir = "05-Amazon_MTurk_expert_response_30cow_pairwise/results/NV" # this can be "../results/DW", "../results/WS", "../results/HE" depending on different worker
+output_dir = "05-Amazon_MTurk_expert_response_30cow_pairwise/results/SB" # this can be "../results/DW", "../results/WS", "../results/HE" depending on different worker
 
 """
 ###############################################################################
@@ -59,12 +59,11 @@ create_hits_in_live = False
 #create_hits_in_live = True
 track_ip = False
 max_worker_num = 20
-# html_files = sorted(filter(lambda f: f.endswith(".html") and f.startswith("HIT"), os.listdir(input_dir)))
-html_files = ['HIT10.html', 'HIT11.html']
+html_files = sorted(filter(lambda f: f.endswith(".html") and f.startswith("HIT"), os.listdir(input_dir)))
 master_submitted_tasks_tracker = process_html_files(html_files, input_dir, create_hits_in_live, key_dir, max_worker_num)
 master_submitted_tasks_tracker = master_submitted_tasks_tracker.sort_values(by='HIT').reset_index(drop=True)
 
-master_submitted_tasks_tracker.to_csv(os.path.join(output_dir, ('all_submitted_tracker_nina_resub' + today + '.csv')))
+master_submitted_tasks_tracker.to_csv(os.path.join(output_dir, ('all_submitted_tracker_SB_' + today + '.csv')))
 
 # a for loop to print out the HIT address of all HITs in master_submitted_tasks_tracker, in the format of "HIT0: address"
 for index, row in master_submitted_tasks_tracker.iterrows():
@@ -80,7 +79,7 @@ for index, row in master_submitted_tasks_tracker.iterrows():
 """
 
 # read in submitted tasks
-master_submitted_tasks_tracker = pd.read_csv(os.path.join(output_dir, ("all_submitted_tracker_nina_resubSep-22-2023.csv")))
+master_submitted_tasks_tracker = pd.read_csv(os.path.join(output_dir, ("all_submitted_tracker_SB_Oct-01-2023.csv")))
 
 # Create a client
 client, mturk_environment = create_mturk_client(create_hits_in_live, key_dir)
@@ -143,10 +142,10 @@ for index, row in master_submitted_tasks_tracker.iterrows():
 
 
 # Save DataFrames to CSV files
-master_worker_response_tracker.to_csv(os.path.join(output_dir, ('master_worker_response_tracke_resub_' + today + '.csv')), index=False)
-master_approved_responses.to_csv(os.path.join(output_dir, ('master_approved_responses_resub_' + today + '.csv')), index=False)
-master_rejected_responses.to_csv(os.path.join(output_dir, ('master_rejected_responses_resub_' + today + '.csv')), index=False)
-master_all_responses.to_csv(os.path.join(output_dir, ('master_all_responses_resub_' + today + '.csv')), index=False)
+master_worker_response_tracker.to_csv(os.path.join(output_dir, ('master_worker_response_tracke_ruan_r1_' + today + '.csv')), index=False)
+master_approved_responses.to_csv(os.path.join(output_dir, ('master_approved_responses_ruan_r1_' + today + '.csv')), index=False)
+master_rejected_responses.to_csv(os.path.join(output_dir, ('master_rejected_responses_ruan_r1_' + today + '.csv')), index=False)
+master_all_responses.to_csv(os.path.join(output_dir, ('master_all_responses_ruan_r1_' + today + '.csv')), index=False)
 
 
 
